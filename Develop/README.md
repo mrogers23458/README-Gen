b0e8ad03633edd7a8f196ebc65150a0e84ae8109
Mike Rogers <mrogers23458@gmail.com>
2021-08-22 21:57:53 -0700

    removed coniditional to create table of contents, felt clunky

--- Develop/README.md
+++ Develop/README.md
@@ -1,6 +1,9 @@
 
-    ## some title
-    ## Description
-    nope
-    ## Table of contents
-    - one-  two-  three
+# title
+## Description
+desc
+## Table of contents
+1 one
+2 two
+3 three
+4 foud
--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,7 @@ iq = require('inquirer')
 fs = require('fs')
 console.log(iq)
 console.log(fs)
+
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -14,12 +15,16 @@ const questions = [{
     name: 'description'
 },{
     type: 'input',
-    message: 'Enter Table of Contents items',
-    name: 'toc'
+    message: 'Enter Table of Contents items separated by commas eg; (item1,item2,item3, ...): ',
+    name: 'toc',
 },{
     type: 'input',
     message: 'Enter installation instructions',
     name: 'installation'
+},{
+    type: 'input',
+    message: 'Enter usage instructions',
+    name: 'usage'
 },{
     type: 'input',
     message: 'Enter usage instructions',
@@ -43,31 +48,15 @@ const questions = [{
 }];
 
 // TODO: Create a function to write README file
-const writeReadMe = ({title, description, toc}) => 
-`
-## ${title}
-## Description
-${description}
-## Table of contents
-${toc}`
-
 function writeToFile (res){
     const tocList = res.toc.split(',').map((item, i) => `${i+1} ${item}\n`).join('')
-    ['one', 'two', 'three']
-    ['- one', '- two', '-three']
-    ` -one 
-        -two
-        -three`
+
    return `
-    ## ${res.title}
-    ## Description
-    ${res.description}
-    ## Table of contents
-    ${tocList}
-    
-    <li>toc item1</li>
-    <li>toc item2</li>
-    <li>toc item3</li>`
+# ${res.title}
+## Description
+${res.description}
+## Table of contents
+${tocList}`
 }
 
 // TODO: Create a function to initialize app
@@ -75,13 +64,10 @@ function init() {
     iq.prompt(questions)
     .then(function(response){
         console.log(response)
-        const filename = response.title
-        console.log(filename)
-
+       
         //fs function to write file
         fs.writeFile(`README.md`, writeToFile(response), function(err){
             if (err) return console.log(err);
-            console.log('success')
         })
 
 
